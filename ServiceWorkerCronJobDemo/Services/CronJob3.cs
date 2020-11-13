1c4fde9edc8aeb794527d2fef2fa27faec6c32cf
Heinrich van Vught <heinrich.vanvught@angloamerican.com>
2020-11-13 14:11:36 +0000

    first

---
+++ ServiceWorkerCronJobDemo/Services/CronJob3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace ServiceWorkerCronJobDemo.Services
+{
+    public class CronJob3 : BaseCronJob
+    {
+        private readonly ILogger<CronJob3> _logger;
+
+        public CronJob3(IScheduleConfig<CronJob3> config, ILogger<CronJob3> logger)
+            : base(config.CronExpression, config.TimeZoneInfo)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CronJob 3 starts.");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task DoWork(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"{DateTime.Now:hh:mm:ss} CronJob 3 is working.");
+            return Task.CompletedTask;
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("CronJob 3 is stopping.");
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
